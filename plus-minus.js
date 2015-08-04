50b2b6e610ae5198cfee6dd5ba5a20e8475f9e77
Jon Bloomer <jon.bloomer@iag.com.au>
2015-08-04 14:57:31 +1000

    tidied up var names and added object interface for cleaner functionality

--- plus-minus.js
+++ plus-minus.js
@@ -1,31 +1,49 @@
+"use strict";
 var PlusMinus = (function () {
-    function PlusMinus(el, pmValues, cb, description, iconClasses, siteTrackingKey) {
+    function PlusMinus(plusMinusObj) {
         this.counter = 0;
-        this.pm_instance = document.getElementById(el);
-        this.values = pmValues;
-        this.descriptionText = description;
-        this.iconClassNames = iconClasses;
-        this.sitetrackingkey = siteTrackingKey;
-        this.callback = cb;
+        this.pmInstance = document.getElementById(plusMinusObj.element);
+        if (plusMinusObj.values && plusMinusObj.values !== '') {
+            this.values = plusMinusObj.values.split(',');
+        }
+        else {
+            this.values = [0, 1, 2, 3, 4];
+        }
+        this.descriptionText = plusMinusObj.description;
+        this.iconClassNames = plusMinusObj.iconClasses;
+        this.sitetrackingkey = plusMinusObj.siteTrackingKey;
+        this.callback = plusMinusObj.cb;
         this.output = this.values[0];
-        this.renderComponent(el);
+        this.renderComponent();
         this.updateOutput();
         this.initEventHandlers();
+        this.pmInit = true;
     }
-    PlusMinus.prototype.renderComponent = function (el) {
+    PlusMinus.prototype.renderComponent = function () {
+        var descriptionHtml;
+        if (this.descriptionText !== '' || this.iconClassNames !== '') {
+            descriptionHtml = '<div class="plus-minus-description">';
+            if (this.iconClassNames !== '') {
+                descriptionHtml += '<i class="icon icon-custom ' + this.iconClassNames + '"></i>';
+            }
+            if (this.descriptionText !== '') {
+                descriptionHtml += '<p class="plus-minus-description-text txt-default">' + this.descriptionText + '</p>';
+            }
+            descriptionHtml += '</div>';
+        }
+        this.pmInstance.insertAdjacentHTML('afterend', descriptionHtml);
         var componentHtml;
-        componentHtml = '<div class="plusMinusWidget"><a href="#" class="btn minus">-</a> <input class="plus-minus-output" name="output" value="" readonly/> <a href="#" class="btn plus">+</a></div>';
-        this.pm_instance.innerHTML = componentHtml;
+        componentHtml = '<a href="#" class="plus-minus-button plus-minus-button-minus"><i class="icon icon-minus">-</i></a><input class="plus-minus-output" type="text" name="" value="" readonly="readonly"><a href="#" class="plus-minus-button plus-minus-button-plus"><i class="icon icon-plus">+</i></a>';
+        this.pmInstance.innerHTML = componentHtml;
     };
     PlusMinus.prototype.initEventHandlers = function () {
         var _this = this;
-        var btn_minus = this.pm_instance.querySelector('.minus');
-        var self = this;
+        var btn_minus = this.pmInstance.querySelector('.plus-minus-button-minus');
         btn_minus.onclick = function (e) {
             console.log('click minus');
             _this.decrement();
         };
-        var btn_plus = this.pm_instance.querySelector('.plus');
+        var btn_plus = this.pmInstance.querySelector('.plus-minus-button-plus');
         btn_plus.onclick = function (e) {
             console.log('click plus');
             _this.increment();
@@ -47,9 +65,11 @@ var PlusMinus = (function () {
     };
     PlusMinus.prototype.updateOutput = function () {
         this.output = this.values[this.counter];
-        var pm_output = this.pm_instance.querySelector('.plus-minus-output');
+        var pm_output = this.pmInstance.querySelector('.plus-minus-output');
         pm_output.value = this.values[this.counter];
-        this.notifyCallback();
+        if (this.pmInit) {
+            this.notifyCallback();
+        }
     };
     PlusMinus.prototype.notifyCallback = function () {
         console.log('notify');
--- plus-minus.ts
+++ plus-minus.ts
@@ -1,3 +1,14 @@
+"use strict";
+
+interface PlusMinusObj {
+  element:string; // id of dom element
+  values:string; // comma seperated string to convert to an array
+  description:string; // displayed next to component
+  iconClasses:string; // to display an icon
+  cb:any; // callback
+  siteTrackingKey:string;
+}
+
 class PlusMinus {
 
     values: any[];
@@ -7,38 +18,59 @@ class PlusMinus {
     callback: any;
     counter: number;
     output: any;
-    pm_instance: any;
+    pmInstance: any;
+    pmInit: boolean;
 
-    constructor(el:string, pmValues:number[], cb:any, description:string, iconClasses:string, siteTrackingKey:string) {
+    constructor(plusMinusObj: PlusMinusObj) {
       this.counter = 0;
-      this.pm_instance = document.getElementById(el);
-      this.values = pmValues;
-      this.descriptionText = description;
-      this.iconClassNames = iconClasses;
-      this.sitetrackingkey = siteTrackingKey
-      this.callback = cb;
+      this.pmInstance = document.getElementById(plusMinusObj.element);
+
+      if (plusMinusObj.values && plusMinusObj.values !== '') {
+        this.values = plusMinusObj.values.split(',');
+      }else{
+        this.values = [0,1,2,3,4];
+      }
+      this.descriptionText = plusMinusObj.description;
+      this.iconClassNames = plusMinusObj.iconClasses;
+      this.sitetrackingkey = plusMinusObj.siteTrackingKey
+      this.callback = plusMinusObj.cb;
       this.output = this.values[0];
 
       // initialise the component
-      this.renderComponent(el); // render the html for the component
+      this.renderComponent(); // render the html for the component
       this.updateOutput(); // fill the output field with array value based on counter value
       this.initEventHandlers(); // add event handlers to buttons
+      this.pmInit = true;
     }
 
-    private renderComponent(el:any):void {
+    private renderComponent():void {
+        var descriptionHtml:string;
+
+        if (this.descriptionText !== '' || this.iconClassNames !== '') {
+          descriptionHtml = '<div class="plus-minus-description">';
+          if (this.iconClassNames !== '') {
+            descriptionHtml += '<i class="icon icon-custom '+this.iconClassNames+'"></i>';
+          }
+          if (this.descriptionText !== '') {
+  					descriptionHtml += '<p class="plus-minus-description-text txt-default">'+this.descriptionText+'</p>';
+  				}
+          descriptionHtml += '</div>';
+        }
+
+        this.pmInstance.insertAdjacentHTML('afterend', descriptionHtml);
+
         var componentHtml: string;
-        componentHtml = '<div class="plusMinusWidget"><a href="#" class="btn minus">-</a> <input class="plus-minus-output" name="output" value="" readonly/> <a href="#" class="btn plus">+</a></div>';
-        this.pm_instance.innerHTML = componentHtml;
+        componentHtml = '<a href="#" class="plus-minus-button plus-minus-button-minus"><i class="icon icon-minus">-</i></a><input class="plus-minus-output" type="text" name="" value="" readonly="readonly"><a href="#" class="plus-minus-button plus-minus-button-plus"><i class="icon icon-plus">+</i></a>';
+        this.pmInstance.innerHTML = componentHtml;
     }
 
     private initEventHandlers():void {
-      var btn_minus = this.pm_instance.querySelector('.minus');
-      var self = this;
+      var btn_minus = this.pmInstance.querySelector('.plus-minus-button-minus');
       btn_minus.onclick = (e) => {
         console.log('click minus');
         this.decrement();
       }
-      var btn_plus = this.pm_instance.querySelector('.plus');
+      var btn_plus = this.pmInstance.querySelector('.plus-minus-button-plus');
       btn_plus.onclick = (e) => {
         console.log('click plus');
         this.increment();
@@ -69,9 +101,11 @@ class PlusMinus {
 
     private updateOutput():void {
       this.output = this.values[this.counter];
-      var pm_output = this.pm_instance.querySelector('.plus-minus-output');
+      var pm_output = this.pmInstance.querySelector('.plus-minus-output');
       pm_output.value = this.values[this.counter];
-      this.notifyCallback();
+      if (this.pmInit) {
+        this.notifyCallback();
+      }
     }
 
     /**
